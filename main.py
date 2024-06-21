83ecb7e2dff346d2c02331c358b1c05ba5c7dbf4
tanvi424 <tanvimengade424@gmail.com>
2024-06-21 11:51:57 +0530

    Second Commit

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-import streamlit as st 
-import os
-import google.generativeai as genai
+# import streamlit as st 
+# import os
+# import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAGCH5nhbiIuOQgaxWSt8TzyjqN7qjO_BY")
-model = genai.GenerativeModel('gemini-pro')
+# genai.configure(api_key="AIzaSyAGCH5nhbiIuOQgaxWSt8TzyjqN7qjO_BY")
+# model = genai.GenerativeModel('gemini-pro')
 
-st.set_page_config(page_title="Chatbot")
-# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
+# st.set_page_config(page_title="Chatbot")
+# # Gemini uses 'model' for assistant; Streamlit uses 'assistant'
 
-def role_to_streamlit(role):
-  if role == "model":
-    return "assistant"
-  else:
-    return role
+# def role_to_streamlit(role):
+#   if role == "model":
+#     return "assistant"
+#   else:
+#     return role
   
-# Add a Gemini Chat history object to Streamlit session state
+# # Add a Gemini Chat history object to Streamlit session state
   
-if "chat" not in st.session_state:
-    st.session_state.chat = model.start_chat(history = [])
+# if "chat" not in st.session_state:
+#     st.session_state.chat = model.start_chat(history = [])
 
-# Display Form Title
-st.title("Chat With Tanvi !")
+# # Display Form Title
+# st.title("Chat With Tanvi !")
 
-# Display chat messages from history above current input box
-for message in st.session_state.chat.history:
-    with st.chat_message(role_to_streamlit(message.role)):
-        st.markdown(message.parts[0].text)
+# # Display chat messages from history above current input box
+# for message in st.session_state.chat.history:
+#     with st.chat_message(role_to_streamlit(message.role)):
+#         st.markdown(message.parts[0].text)
 
-# Accept user's next message, add to context, resubmit context to Gemini
-if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
-    # Display user's last message
-    st.chat_message("user").markdown(prompt)
+# # Accept user's next message, add to context, resubmit context to Gemini
+# if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
+#     # Display user's last message
+#     st.chat_message("user").markdown(prompt)
     
-    # Send user entry to Gemini and read the response
-    response = st.session_state.chat.send_message(prompt) 
+#     # Send user entry to Gemini and read the response
+#     response = st.session_state.chat.send_message(prompt) 
     
-    # Display last 
-    with st.chat_message("assistant"):
-        st.markdown(response.text)
+#     # Display last 
+#     with st.chat_message("assistant"):
+#         st.markdown(response.text)
 
        
        
